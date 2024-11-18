85793f92a46724a2816f6768c9f131171d5aea8a
Github MMI3 <github@mmi-troyes.fr>
2024-11-18 08:21:17 +0000

    feat: correct errors phpstan and phpcs

--- src/Controller/ProductController.php
+++ src/Controller/ProductController.php
@@ -13,25 +13,34 @@ class ProductController extends AbstractController
     #[Route('/product', name: 'app_product')]
     public function index(): Response
     {
-        return $this->render('product/index.html.twig', [
+        return $this->render(
+            'product/index.html.twig',
+            [
             'controller_name' => 'ProductController',
-        ]);
+            ]
+        );
     }
 
     #[Route('/products', name: 'product_list')]
     public function listProducts(): Response
     {
-        return $this->render('product/list.html.twig', [
+        return $this->render(
+            'product/list.html.twig',
+            [
             'controller_name' => 'ProductController',
-        ]);
+            ]
+        );
     }
 
     #[Route('/product/{id}', name: 'product_view')]
     public function viewProduct(Request $request, $id): Response
     {
-        return $this->render('product/view.html.twig', [
+        return $this->render(
+            'product/view.html.twig',
+            [
             'controller_name' => 'ProductController',
             'product_id' => $id,
-        ]);
+            ]
+        );
     }
 }
